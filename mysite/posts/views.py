bc567b4ed592d5ade93d4dc016d1c9acecc77a3e
sooho72 <rhkdgks72@naver.com>
2024-12-31 16:18:47 +0900

    게시글 등록 만드는중

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -1,12 +1,30 @@
 from django.http import HttpResponse
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Posts
+
+from django.contrib import messages
+from .form import PostCreateFrom
 
 # Create your views here.
 # 게시글 등록
+# def create_post(request):
+#     return HttpResponse('게시글 등록')
 def create_post(request):
-    return HttpResponse('게시글 등록')
+    form = PostCreateFrom()
+
+    if request.method == 'POST' :
+        form = PostCreateFrom(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            messages.success(request,'게시글이 등록되었습니다.')
+            return redirect("posts:read", post_id = post.id)
+        else:
+            messages.error(request, '게시글 등록에 실패했습니다.')
+
+        return render(request,'posts/create.html',{'form' : form})
 
 # 게시글 보기
 def get_post(request, post_id):
